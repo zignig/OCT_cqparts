2d5e0114cc68da27ddcdb43aa0d00513bba3d1ca
Simon Kirkby <obeygiantrobot@gmail.com>
2020-03-29 09:09:12 +0800

    comments in fetch

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,13 @@
+echo Fetch Cadquery OCP
 wget https://github.com/CadQuery/OCP/releases/download/7.4-alpha/OCP.cpython-36m-x86_64-linux-gnu.so
+echo Fetch pywarap
 git clone https://github.com/CadQuery/pywrap.git
-git clone https://git.dev.opencascade.org/repos/occt.git refs/tags/V7_4_0
+echo Install Packages (incomplete)
 sudo apt-get install tcllib tklib tcl-dev tk-dev libfreetype-dev libxt-dev libxmu-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libfreeimage-dev libtbb-dev libtommath1
+echo Download OCCT
+git clone https://git.dev.opencascade.org/repos/occt.git refs/tags/V7_4_0
+echo Build OCCT , will take some time
 cd occt
 cmake .
 make
+
